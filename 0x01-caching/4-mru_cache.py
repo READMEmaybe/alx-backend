ce96d187b1a156bbcc8288846d8b9e9146e84eff
READMEmaybe <haroune11411@gmail.com>
2024-05-09 01:20:32 +0100

    Add task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" MRU Caching """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRU caching system """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if (len(self.cache_data) >= BaseCaching.MAX_ITEMS) and \
+                   (key not in self.cache_data):
+                discard = self.queue.pop()
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+            self.cache_data[key] = item
+            if key in self.queue:
+                self.queue.remove(key)
+            self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None
